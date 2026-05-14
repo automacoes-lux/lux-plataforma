baa69905c9655e27df9d001b3f134b4c8fd62f72
automacoes-lux <automacoes@luxmarketing.com.br>
2026-05-14 17:52:07 -0300

    Update shell.js

--- js/shell.js
+++ js/shell.js
@@ -8,6 +8,7 @@ const TOOLS = {
   figurinhas: 'tools/figurinhas.html',
   musica:     'tools/maestro.html',
   videos:     'tools/videos.html',
+  'meus-custos': 'tools/meus-custos.html',
   admin:      'tools/admin.html',
 };
 
